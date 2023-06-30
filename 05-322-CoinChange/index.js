6823b6912e50e80d7eb07e4a4a99ef35c935ec93
abraham <abraham.mathewos.meja@gmail.com>
2023-06-30 08:34:12 -0500

    Add solution to coinChange problem

---
+++ 05-322-CoinChange/index.js
@@ -0,0 +1,18 @@
+var coinChange = function (coins, amount) {
+  let dp = Array(amount + 1).fill(Infinity);
+
+  // base case
+  dp[0] = 0;
+
+  for (let curAmount = 1; curAmount <= amount; curAmount++) {
+    for (let coin of coins) {
+      if (curAmount - coin >= 0) {
+        dp[curAmount] = Math.min(dp[curAmount], 1 + dp[curAmount - coin]);
+      }
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 11));
+console.log(coinChange([5, 6], 2));
