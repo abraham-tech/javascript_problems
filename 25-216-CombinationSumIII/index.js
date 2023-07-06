92048fef5df915d54de071c07af5bd234c3ce11f
abraham <abraham.mathewos.meja@gmail.com>
2023-07-06 09:14:57 -0500

    Add solution to Combination Sum III

---
+++ 25-216-CombinationSumIII/index.js
@@ -0,0 +1,19 @@
+const combinationSum3 = function(k, n){
+    let result = [];
+
+    function dfs(index, current, total){
+        if(total < 0 || current.length > k) return;
+        if(total === 0 && current.length === k) {
+            result.push([...current]);
+        }
+        for(let i = index; i <= 9; i++){
+            current.push(i);
+            dfs(i+1, current, total-i);
+            current.pop();
+        }
+    }
+    dfs(1,[],n);
+    return result;
+}
+
+console.log(combinationSum3(3,7));
