80d1f64314bdd51caab1430155be77824ac17de0
abraham <abraham.mathewos.meja@gmail.com>
2023-11-22 22:23:12 -0600

    Add rotated sorted array solution

---
+++ 40-153-Rotated-Sorted-Array/index.js
@@ -0,0 +1,18 @@
+const findMin = function(nums){
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left < right) {
+        let mid = Math.floor((right + left) / 2);
+
+        if(nums[right] < nums[mid]){
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
+
+console.log(findMin([3,4,5,1,2]));
+console.log(findMin([4,5,67,0,1,2,3]));
