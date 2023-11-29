68097236b2cb455cbccc307c62e9286cf9e37069
abraham <abraham.mathewos.meja@gmail.com>
2023-11-28 21:59:21 -0600

    Add group anagrams

---
+++ 46-49-GroupAnagrams/index.js
@@ -0,0 +1,13 @@
+const groupAnagrams = function(stars) {
+    let sorted = stars.map(star => star.split("").sort().join(""));
+    let mymap = {};
+    sorted.forEach((star, i) => {
+        if(!mymap[star]){
+            mymap[star] = [stars[i]]
+        }
+        mymap[star].push(stars[i])
+    });
+    return Object.values(mymap);
+}
+
+console.log(groupAnagrams(["eat", "tea","tan","ate","nat","bat"]));
