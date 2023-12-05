9a557f4e2583eb182a2a8993ad902376f1ec4ffb
abraham <abraham.mathewos.meja@gmail.com>
2023-12-05 15:27:48 -0600

    Rob a house prob solution

---
+++ 55-198-HouseRobber/index.js
@@ -0,0 +1,15 @@
+const rob = function(nums){
+    let even = 0;
+    let odd = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(i % 2 === 0){
+            even += nums[i];
+        }else{
+            odd +=nums[i];
+        }
+    }
+    return Math.max(even, odd);
+}
+
+
+console.log(rob([2,7,9,3,1]))
