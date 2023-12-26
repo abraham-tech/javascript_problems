d433d51b2589e361a945f1a9a2472b38bec597c2
abraham <abraham.mathewos.meja@gmail.com>
2023-12-26 16:05:23 -0600

    Add solution for valid path problem

---
+++ 74-1971-FindIfPathExistsInGraph/index.js
@@ -0,0 +1,34 @@
+const validPath = function (n, edges, source, destination){
+    let graph = new Map();
+    let visited = new Set();
+
+    for(let [v,e] of edges){
+        if(graph.has(v)){
+            graph.get(v).push(e);
+        }else {
+            graph.set(v, [e]);
+        }
+        if(graph.has(e)){
+            graph.get(e).push(v);
+        } else {
+            graph.set(e, [v]);
+        }
+    }
+
+    function dfs(vertex){
+        visited.add(vertex);
+        let neighbours = graph.get(vertex);
+        if(neighbours && neighbours.length > 0){
+            for(let i = 0; i < neighbours.length; i++){
+                if(!visited.has(neighbours[i])){
+                    dfs(neighbours[i]);
+                }
+            }
+        }
+    }
+
+    dfs(source);
+    return visited.has(destination);
+}
+
+console.log(validPath(3,[[0,1],[1,2],[2,0]],0,2))
