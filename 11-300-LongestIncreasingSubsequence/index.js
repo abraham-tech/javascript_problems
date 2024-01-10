a94a500101f48008f01e8284e69d817c3573d9a4
abraham <abraham.mathewos.meja@gmail.com>
2024-01-09 22:23:47 -0600

    Refactor

--- 11-300-LongestIncreasingSubsequence/index.js
+++ 11-300-LongestIncreasingSubsequence/index.js
@@ -1,11 +1,9 @@
-const lengthOfLIS = function (nums) {
-    let dp = nums.map(n => 1);
+const lengthOfLIS = nums => {
+    const dp = new Array(nums.length).fill(1);
 
     for(let i = 1; i < nums.length; i++){
-        for(j=0; j < i; j++){
-            if(nums[i] > nums[j]){
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-            }
+        for(let j = 0; j < i; j++){
+            if(nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
         }
     }
     return Math.max(...dp);
