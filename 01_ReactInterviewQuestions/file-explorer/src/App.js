9ab08cd54bebd9650a94af6dc19f6776886521f0
abraham <abraham.mathewos.meja@gmail.com>
2024-01-24 21:01:08 -0600

    Add file and folder creaion functionality

--- 01_ReactInterviewQuestions/file-explorer/src/App.js
+++ 01_ReactInterviewQuestions/file-explorer/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Folder from "./components/Folder";
 import explorer from "./data/folderData";
 import {useState} from "react";
+import useTreeTraverse from "./hooks/useTreeTraverse";
 
 function App() {
     const [explorerData, setExprolrerData] = useState(explorer);
+    const { insertNode } = useTreeTraverse();
+    const handleInsertNode = (folderId, item, isFolder) => {
+        const finalTree = insertNode(explorerData, folderId, item, isFolder);
+        setExprolrerData(finalTree)
+    }
   return (
     <div className="App">
-        <Folder explorer={explorerData}/>
+        <Folder handleInsertNode={handleInsertNode} explorer={explorerData}/>
     </div>
   );
 }
--- 01_ReactInterviewQuestions/file-explorer/src/components/Folder.js
+++ 01_ReactInterviewQuestions/file-explorer/src/components/Folder.js
@@ -1,22 +1,57 @@
 import {useState} from "react";
-import explorer from "../data/folderData";
 
-const Folder = ({explorer}) => {
+const Folder = ({handleInsertNode, explorer}) => {
     const [expand, setExpand] = useState(false);
+    const [showInput, setShowInput] = useState( {
+        isFolder: null,
+        visible: false
+    })
+
+    const handleAddItem = (e, isFolder) => {
+        e.stopPropagation();
+        setExpand(true);
+        setShowInput({
+            isFolder: isFolder,
+            visible: true
+        })
+    }
+
+    const onSaveItem = (e) => {
+        if(e.keyCode === 13 && e.target.value) {
+            handleInsertNode(explorer.id, e.target.value, showInput.isFolder)
+            setShowInput({...showInput, visible: false});
+        }
+    }
     if(explorer.isFolder){
         return (
             <div style={{marginTop: "5px"}}>
 
                 <div onClick={() => setExpand(!expand)} className="folder">
                     <span>📁 {explorer.name}</span>
+                    <div>
+                        <button onClick={(e) => handleAddItem(e, true)}> Folder +</button>
+                        <button onClick={(e) => handleAddItem(e, false)}> File +</button>
+                    </div>
                 </div>
-                <div style={{ display: expand? "block": "none", paddingLeft:25 }}>
-                {explorer.items.map(exp => {
-                    return (
-                        <Folder key={exp.id} explorer={exp}/>
-                    )
-                })}
-                </div>
+                {expand && (<div style={{ paddingLeft: 25}}>
+                    {showInput.visible && (
+                        <div>
+                            <span> {showInput.isFolder ? "📁" : "📄"}</span>
+                            <input
+                                type="text"
+                                onBlur={() => setShowInput({...showInput, visible: false})}
+                                autoFocus
+                                onKeyDown={onSaveItem}
+                            />
+                        </div>
+                    )}
+                    {explorer.items.map(exp => {
+                        return (
+                            <Folder handleInsertNode={handleInsertNode} key={exp.id} explorer={exp}/>
+                        )
+                    })}
+                </div>)}
+
             </div>
         )
     } else {
