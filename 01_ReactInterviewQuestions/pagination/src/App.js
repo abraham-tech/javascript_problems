49985f77c9d41c0b0da042c32c3fd5afc62627d3
Abraham Meja <abraham@Abrahams-MacBook-Pro.local>
2024-01-25 11:35:53 -0600

    Add simple frontend pagination.

--- 01_ReactInterviewQuestions/pagination/src/App.js
+++ 01_ReactInterviewQuestions/pagination/src/App.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from 'react';
 
 function App() {
     const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
+    const PAGE = [...Array(products.length / 10 )];
     const fetchProducts = async () => {
         try {
             const response = await fetch('https://dummyjson.com/products?limit=100');
@@ -21,23 +23,32 @@ function App() {
     useEffect(() => {
         fetchProducts();
     }, [])
+
+    if(products.length <= 0){
+        return (
+            <p>No products to display</p>
+        )
+    }
     return (
-        <div>
-            {products.length > 0 && (
-                <div className="products">
-                    {products.map((product, i) => {
-                        return (
-                            <span className="products__single" key={i}>
-                                <img src={product.thumbnail} alt="logo"/>
-
-                                    <b>{product.brand} </b>
-
-                            </span>)
-                    })}
-                </div>
-            )}
-
+        <>
+        <div className="products">
+            {products.slice(page * 10 - 10, page * 10).map((product, i) => {
+                return (
+                    <span className="products__single" key={i}>
+                        <img src={product.thumbnail} alt="logo"/>
+                        <b>{product.brand} </b>
+                    </span>)
+            })}
         </div>
+            <div className="pagination">
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>⬅️</button>
+                {PAGE.map((_, i) => {
+                    return <span className={page === i + 1? "pagination__selected": ""} onClick={() => setPage(i + 1)}> {i + 1}  </span>
+                }
+        )}
+                <button disabled={PAGE.length === page} onClick={() => setPage(page + 1)}>➡️</button>
+            </div>
+        </>
     );
 }
 
