54d5f3fe5de9ac72102ce0096be398c8b9e6da1d
abraham <abrahammathewos.meja.5695@student.uu.se>
2024-02-06 22:22:22 -0600

    Add solution to top k frequent element

---
+++ 100-347-TopKFrequentElement/index.js
@@ -0,0 +1,29 @@
+const topKFrequent = function(nums, k){
+    let map = {};
+    let bucket = [];
+    let result = [];
+
+    for(let i = 0; i < nums.length; i++){
+        if(!map[nums[i]]){
+            map[nums[i]] = 1;
+        }else {
+            map[nums[i]] += 1;
+        }
+    }
+
+    for(let [num, freq] of Object.entries(map)){
+        if(!bucket[freq]){
+            bucket[freq] = new Set().add(num);
+        } else {
+            bucket[freq] = bucket[freq].add(num)
+        }
+    }
+
+    for(let i = bucket.length - 1; i >= 0; i--){
+        if(bucket[i]) result.push(...bucket[i]);
+        if(result.length === k) break;
+    }
+    return result;
+}
+
+console.log(topKFrequent([1,1,1,2,2,3], 2));
